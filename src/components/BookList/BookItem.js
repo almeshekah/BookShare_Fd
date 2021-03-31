d2a1edc024532722e3248f8bebd3d1211723dd06
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-03-31 10:24:53 +0300

    list grid

--- src/components/BookList/BookItem.js
+++ src/components/BookList/BookItem.js
@@ -16,7 +16,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const BookItem = ({ book }) => {
 	const useStyles = makeStyles(() => ({
 		root: {
-			maxWidth: 345,
+			maxWidth: 250,
 		},
 		media: {
 			height: 0,
@@ -30,40 +30,31 @@ const BookItem = ({ book }) => {
 	const classes = useStyles();
 	return (
 		<>
-			<Grid
-				container
-				spacing={5}
-				direction="row"
-				alignItems="center"
-				justify="center"
-			>
-				<Grid item xs={3}>
-					<Card className={classes.root}>
-						<CardHeader
-							avatar={
-								<Link to={`/otherprofile/${book.userId}`}>
-									<Avatar aria-label="book" className={classes.avatar}>
-										R
-									</Avatar>
-								</Link>
-							}
-							action={
-								<IconButton aria-label="more">
-									<DetailDialog bookId={book.id} />
-								</IconButton>
-							}
-							title={book.name}
-							subheader={book.author}
-						/>
+			<Grid item xs={3}>
+				<Card className={classes.root}>
+					<CardHeader
+						avatar={
+							<Link to={`/otherprofile/${book.userId}`}>
+								<Avatar aria-label="book" className={classes.avatar}>
+									R
+								</Avatar>
+							</Link>
+						}
+						action={
+							<IconButton aria-label="more">
+								<DetailDialog bookId={book.id} />
+							</IconButton>
+						}
+						title={book.name}
+					/>
 
-						<CardMedia
-							className={classes.media}
-							image={book.image}
-							title={book.name}
-						/>
-						<CardContent>Listed for {book.type}</CardContent>
-					</Card>
-				</Grid>
+					<CardMedia
+						className={classes.media}
+						image={book.image}
+						title={book.name}
+					/>
+					<CardContent>Listed for {book.type}</CardContent>
+				</Card>
 			</Grid>
 		</>
 	);
--- src/components/BookList/styles.js
+++ src/components/BookList/styles.js
@@ -1,40 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ListWrapper = styled.div`
-  /* align-items: center;
+	/* align-items: center;
 	justify-content: center;
 	display: flex;
 	flex-wrap: wrap;
 	margin-top: 80px; */
-  align-items: flex-end;
-  display: flex;
-  height: 831px;
-  overflow: hidden;
-  width: 861px;
+	align-items: flex-end;
+	display: flex;
+	height: 831px;
+	overflow: hidden;
+	width: 861px;
 `;
 
 export const Card = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 30%;
-  border-radius: 15px;
-  flex-direction: column;
-  position: relative;
-  transform: scale(0.8);
-  transition: all 0.3s ease-in-out;
-  margin-bottom: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 30%;
+	border-radius: 15px;
+	flex-direction: column;
+	position: relative;
+	transform: scale(0.8);
+	transition: all 0.3s ease-in-out;
+	margin-bottom: 50px;
 `;
 
 export const Img = styled.img`
-  margin-top: -5vw;
-  img {
-    width: 200px;
-  }
+	margin-top: -5vw;
+	img {
+		width: 200px;
+	}
 `;
 
 export const CardHeader = styled.div`
-  flex-direction: column;
-  color: $white;
-  margin: 25px 0;
+	flex-direction: column;
+	color: $white;
+	margin: 25px;
 `;
