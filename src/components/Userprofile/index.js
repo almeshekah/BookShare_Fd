79eb6f95f7410226dca7ce2968dd5ece920e9ede
robie09 <you@example.com>
2021-03-31 23:47:39 +0300

    fix import deleted by mistake

--- src/components/Userprofile/index.js
+++ src/components/Userprofile/index.js
@@ -1,122 +1,122 @@
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 // Components
 import Loading from "../Loading";
 import BookList from "../BookList";
 // Styling
 import { ItemWrapper, Title, ButtonWrapper } from "./styles";
 import { AddButtonStyled } from "../../styles";
-
+import { viewProfile } from "../../store/actions/authActions";
 
 const Userprofile = () => {
-	const userId = useParams().userId;
+  const userId = useParams().userId;
 
-	const dispatch = useDispatch();
-	useEffect(() => {
-		if (userId) {
-			dispatch(viewProfile(userId));
-		}
-	}, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (userId) {
+      dispatch(viewProfile(userId));
+    }
+  }, []);
 
-	const profile = useSelector((state) => state.authReducer.profile);
-	const loading = useSelector((state) => state.authReducer.loading);
+  const profile = useSelector((state) => state.authReducer.profile);
+  const loading = useSelector((state) => state.authReducer.loading);
 
-	const otheProfile = useSelector((state) => state.authReducer.otheProfile);
-	const otheProfileloading = useSelector(
-		(state) => state.authReducer.otheProfileloading
-	);
-	if (!userId) {
-		if (loading) return <Loading />;
-	} else if (otheProfileloading) return <Loading />;
+  const otheProfile = useSelector((state) => state.authReducer.otheProfile);
+  const otheProfileloading = useSelector(
+    (state) => state.authReducer.otheProfileloading
+  );
+  if (!userId) {
+    if (loading) return <Loading />;
+  } else if (otheProfileloading) return <Loading />;
 
-	return (
-		<>
-			{!userId ? (
-				<>
-					<ItemWrapper>
-						<h1
-							style={{
-								marginBottom: '2%',
-								marginLeft: '2.5%',
-								marginTop: '2%',
-							}}
-						>
-							{profile.username}
-						</h1>
-						<img
-							src={
-								profile.image ??
-								'https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png'
-							}
-							alt={profile.firstName}
-						/>
-						<p>
-							Name: {profile.firstName} {profile.lastName}
-						</p>
-						<p>{`Email: ${profile.email}`}</p>
-						<div className="buttons">
-							<Link to="/profile/edit">
-								<button type="button" className="btn btn-dark ">
-									Edit My Profile
-								</button>
-							</Link>
-						</div>
-					</ItemWrapper>
-					<div>
-						<Title>My Books</Title>
-						<ButtonWrapper>
-							<ButtonWrapper>
-								<Link to={`/books/new`}>
-									<AddButtonStyled>Add books</AddButtonStyled>
-								</Link>
-							</ButtonWrapper>
-						</ButtonWrapper>
-						<BookList books={profile.mybook} />
-					</div>
-				</>
-			) : (
-				<>
-					<ItemWrapper>
-						<h1
-							style={{
-								marginBottom: '2%',
-								marginLeft: '2.5%',
-								marginTop: '2%',
-							}}
-						>
-							{otheProfile.username}
-						</h1>
-						<img
-							src={
-								'https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png'
-							}
-							alt={otheProfile.firstName}
-						/>
-						<p>
-							Name: {otheProfile.firstName} {otheProfile.lastName}
-						</p>
-						<p>{`Email: ${otheProfile.email}`}</p>
-						<div className="buttons"></div>
-					</ItemWrapper>
-					<div>
-						<Title>has Books</Title>
+  return (
+    <>
+      {!userId ? (
+        <>
+          <ItemWrapper>
+            <h1
+              style={{
+                marginBottom: "2%",
+                marginLeft: "2.5%",
+                marginTop: "2%",
+              }}
+            >
+              {profile.username}
+            </h1>
+            <img
+              src={
+                profile.image ??
+                "https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png"
+              }
+              alt={profile.firstName}
+            />
+            <p>
+              Name: {profile.firstName} {profile.lastName}
+            </p>
+            <p>{`Email: ${profile.email}`}</p>
+            <div className="buttons">
+              <Link to="/profile/edit">
+                <button type="button" className="btn btn-dark ">
+                  Edit My Profile
+                </button>
+              </Link>
+            </div>
+          </ItemWrapper>
+          <div>
+            <Title>My Books</Title>
+            <ButtonWrapper>
+              <ButtonWrapper>
+                <Link to={`/books/new`}>
+                  <AddButtonStyled>Add books</AddButtonStyled>
+                </Link>
+              </ButtonWrapper>
+            </ButtonWrapper>
+            <BookList books={profile.mybook} />
+          </div>
+        </>
+      ) : (
+        <>
+          <ItemWrapper>
+            <h1
+              style={{
+                marginBottom: "2%",
+                marginLeft: "2.5%",
+                marginTop: "2%",
+              }}
+            >
+              {otheProfile.username}
+            </h1>
+            <img
+              src={
+                "https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png"
+              }
+              alt={otheProfile.firstName}
+            />
+            <p>
+              Name: {otheProfile.firstName} {otheProfile.lastName}
+            </p>
+            <p>{`Email: ${otheProfile.email}`}</p>
+            <div className="buttons"></div>
+          </ItemWrapper>
+          <div>
+            <Title>has Books</Title>
 
-						<ButtonWrapper>
-							<ButtonWrapper>
-								<Link to={`/requests/new`}>
-									<AddButtonStyled>Request</AddButtonStyled>
-								</Link>
-							</ButtonWrapper>
-						</ButtonWrapper>
-						<BookList books={otheProfile.hasbook} />
-					</div>
-				</>
-			)}
-		</>
-	);
+            <ButtonWrapper>
+              <ButtonWrapper>
+                <Link to={`/requests/new`}>
+                  <AddButtonStyled>Request</AddButtonStyled>
+                </Link>
+              </ButtonWrapper>
+            </ButtonWrapper>
+            <BookList books={otheProfile.hasbook} />
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Userprofile;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
 
 import reducer from "./reducers";
-import { checkForToken } from "./actions/authActions";
+import { checkForToken, fetchUsers } from "./actions/authActions";
 import { fetchCategories } from "./actions/categoryActions";
 import { fetchBook } from "./actions/bookActions";
 
-
-import { fetchRequest } from './actions/requestActions';
+import { fetchRequest } from "./actions/requestActions";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
