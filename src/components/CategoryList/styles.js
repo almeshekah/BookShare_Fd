3f1afd42f2534a86f2a16445b4c442ea70e5a2c5
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-04-06 11:20:11 +0300

    chnaged few styling

--- src/components/CategoryList/styles.js
+++ src/components/CategoryList/styles.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
 export const ListWrapper = styled.div`
-	align-items: center;
-	justify-content: center;
-	display: flex;
-	flex-wrap: wrap;
-	margin-top: 80px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 80px;
+`;
+
+export const CategoryWrapper = styled.div`
+  margin: 20px;
+  img {
+    width: 200px;
+    height: 200px;
+  }
+  p {
+    text-align: center;
+  }
 `;
--- src/components/Request/ViewRequest.js
+++ src/components/Request/ViewRequest.js
@@ -9,38 +9,38 @@ import Loading from "../Loading";
 import RequestItem from "./RequestItem";
 
 const ViewRequest = () => {
-	const requests = useSelector((state) => state.requestReducer.requests);
-	const loading = useSelector((state) => state.requestReducer.loading);
-	if (loading) return <Loading />;
-	const requestList = requests
-		.filter((request) => request.status === 0)
-		.map((request) => <RequestItem request={request} key={request.id} />);
+  const requests = useSelector((state) => state.requestReducer.requests);
+  const loading = useSelector((state) => state.requestReducer.loading);
+  if (loading) return <Loading />;
+  const requestList = requests
+    .filter((request) => request.status === 0)
+    .map((request) => <RequestItem request={request} key={request.id} />);
 
-	return (
-		<>
-			<Typography variant="h3" align="center" color="primary">
-				My Requests
-			</Typography>
-			{requests.length === 0 ? (
-				<>
-					<Typography align="center" color="primary">
-						You have no requests &nbsp;
-					</Typography>
-				</>
-			) : (
-				<Grid
-					container
-					spacing={3}
-					style={{
-						marginLeft: "17em",
-						marginTop: "3em",
-					}}
-				>
-					{requestList}
-				</Grid>
-			)}
-		</>
-	);
+  return (
+    <>
+      <Typography variant="h3" align="center" color="primary">
+        My Requests
+      </Typography>
+      {requests.length === 0 ? (
+        <>
+          <Typography align="center" color="primary">
+            You have no requests &nbsp;
+          </Typography>
+        </>
+      ) : (
+        <Grid
+          container
+          spacing={3}
+          style={{
+            marginLeft: "17em",
+            marginTop: "3em",
+          }}
+        >
+          {requestList}
+        </Grid>
+      )}
+    </>
+  );
 };
 
 export default ViewRequest;
