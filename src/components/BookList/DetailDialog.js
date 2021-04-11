795206dab5da273bc8f45e2164f3664a9de0e8b9
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-04-11 16:08:14 +0300

    changes to book list

--- src/components/BookList/DetailDialog.js
+++ src/components/BookList/DetailDialog.js
@@ -9,89 +9,107 @@ import Dialog from "@material-ui/core/Dialog";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 // Styling
 import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ListAltIcon from "@material-ui/icons/ListAlt";
 
 const styles = (theme) => ({
-	root: {
-		margin: 0,
-		padding: theme.spacing(2),
-	},
-	closeButton: {
-		position: "absolute",
-		right: theme.spacing(1),
-		top: theme.spacing(1),
-		color: theme.palette.grey[500],
-	},
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  moreButton: {
+    marginTop: "1em",
+    marginLeft: "10.5em",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.secondary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
 });
 
 const DialogTitle = withStyles(styles)((props) => {
-	const { children, classes, onClose, ...other } = props;
-	return (
-		<MuiDialogTitle disableTypography className={classes.root} {...other}>
-			<Typography variant="h6">{children}</Typography>
-			{onClose ? (
-				<IconButton
-					aria-label="close"
-					className={classes.closeButton}
-					onClick={onClose}
-				>
-					<CloseIcon />
-				</IconButton>
-			) : null}
-		</MuiDialogTitle>
-	);
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
 });
 
 const DialogContent = withStyles((theme) => ({
-	root: {
-		padding: theme.spacing(2),
-	},
+  root: {
+    padding: theme.spacing(2),
+  },
 }))(MuiDialogContent);
 
 const DetailDialog = ({ bookId, mybook }) => {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const books = useSelector((state) => state.bookReducer.books);
+  const books = useSelector((state) => state.bookReducer.books);
 
-	const book = books.find((book) => book.id === bookId);
+  const book = books.find((book) => book.id === bookId);
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	return (
-		<>
-			<div>
-				<MoreVertIcon
-					variant="outlined"
-					color="primary"
-					onClick={handleClickOpen}
-				></MoreVertIcon>
-				<Dialog
-					onClose={handleClose}
-					aria-labelledby="customized-dialog-title"
-					open={open}
-					fullWidth
-					maxWidth="sm"
-				>
-					<DialogTitle id="customized-dialog-title" onClose={handleClose}>
-						Book Details
-					</DialogTitle>
-					<DialogContent dividers>
-						<CardDetail bookId={book.id} mybook={mybook} />
-					</DialogContent>
-				</Dialog>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <Button
+          variant="contained"
+          onClick={handleClickOpen}
+          style={{
+            backgroundColor: "#413b89",
+            color: "#f3e7dd",
+            marginBottom: "-13em",
+            marginRight: "2em",
+          }}
+          endIcon
+        >
+          Details
+        </Button>
+        <Dialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+          fullWidth
+          maxWidth="sm"
+        >
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Book Details
+          </DialogTitle>
+          <DialogContent dividers>
+            <CardDetail bookId={book.id} mybook={mybook} />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
+  );
 };
 
 export default DetailDialog;
--- src/components/Userprofile/index.js
+++ src/components/Userprofile/index.js
@@ -24,31 +24,26 @@ import ViewRequest from "../Request/ViewRequest";
 // Styling
 import { makeStyles } from "@material-ui/core/styles";
 import { Title } from "./styles";
-import { AddButtonStyled } from "../../styles";
 import MessageIcon from "@material-ui/icons/Message";
 import AddIcon from "@material-ui/icons/Add";
 import * as FaIcons from "react-icons/fa";
 
 const Userprofile = () => {
-	const userId = useParams().userId;
+  const userId = useParams().userId;
 
-	const useStyles = makeStyles((theme) => ({
-		root: {
-			maxWidth: 300,
-			maxHeight: 700,
-			borderRadius: 5,
-			backgroundColor: theme.palette.secondary.main,
-			color: theme.palette.primary.main,
-		},
+  const useStyles = makeStyles((theme) => ({
+    // root: {
 
-		primaryColor: {
-			backgroundColor: theme.palette.primary.main,
-			"&:hover": {
-				background: theme.palette.primary.secondary,
-				color: theme.palette.primary.main,
-			},
-			color: theme.palette.secondary.main,
-		},
+    // },
+
+    primaryColor: {
+      backgroundColor: theme.palette.primary.main,
+      "&:hover": {
+        background: theme.palette.primary.secondary,
+        color: theme.palette.primary.main,
+      },
+      color: theme.palette.secondary.main,
+    },
 
     successColor: {
       backgroundColor: theme.palette.success.main,
@@ -71,9 +66,16 @@ const Userprofile = () => {
       background: theme.palette.primary.main,
     },
     userName: { marginTop: "0.8em", marginBottom: "0.8em" },
+
     card: {
-      marginLeft: "-6em",
-      marginTop: "-83em",
+      maxWidth: 300,
+      maxHeight: 700,
+      borderRadius: 5,
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.primary.main,
+      marginLeft: "6em",
+      marginTop: "-90em",
+      // marginBottom: "-60em",
     },
     otherUserCard: {
       marginRight: "-10em",
@@ -94,129 +96,128 @@ const Userprofile = () => {
     },
   }));
 
+  const classes = useStyles();
 
-	const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (userId) {
+      dispatch(viewProfile(userId));
+    } else {
+    }
+  }, [userId]);
 
-	const dispatch = useDispatch();
-	useEffect(() => {
-		if (userId) {
-			dispatch(viewProfile(userId));
-		} else {
-		}
-	}, [userId]);
+  const categories = useSelector((state) => state.categoryReducer.categories);
+  const profile = useSelector((state) => state.authReducer.profile);
+  const loading = useSelector((state) => state.authReducer.loading);
+  const mybook = useSelector((state) => state.bookReducer.mybook);
 
-	const categories = useSelector((state) => state.categoryReducer.categories);
-	const profile = useSelector((state) => state.authReducer.profile);
-	const loading = useSelector((state) => state.authReducer.loading);
-	const mybook = useSelector((state) => state.bookReducer.mybook);
+  const otheProfile = useSelector((state) => state.authReducer.otheProfile);
+  const otheProfileloading = useSelector(
+    (state) => state.authReducer.otheProfileloading
+  );
+  let hisbook = [];
+  let _mybook = [];
+  let mycategories = [];
+  let hiscategories = [];
 
-	const otheProfile = useSelector((state) => state.authReducer.otheProfile);
-	const otheProfileloading = useSelector(
-		(state) => state.authReducer.otheProfileloading
-	);
-	let hisbook = [];
-	let _mybook = [];
-	let mycategories = [];
-	let hiscategories = [];
+  if (!userId) {
+    if (loading) return <Loading />;
+    _mybook = mybook.filter((book) => profile.userId === book.userId);
+    mycategories = categories
+      .filter((category) => {
+        return profile.myCategory.some((_category) => {
+          return _category.categoryId === category.id;
+        });
+      })
+      .map((category) => category.name);
+  } else {
+    if (otheProfileloading) return <Loading />;
+    else {
+      hisbook = mybook.filter((book) => otheProfile.userId === book.userId);
+      hiscategories = categories
+        .filter((category) => {
+          return otheProfile.hasCategory.some((_category) => {
+            return _category.categoryId === category.id;
+          });
+        })
+        .map((category) => category.name);
+    }
+  }
+  console.log(mycategories);
+  console.log(hiscategories);
+  return (
+    <>
+      {!userId ? (
+        <>
+          <>
+            <ViewRequest />
+            {/* <Grid
+              container
+              spacing={0}
+              direction="row"
+              alignItems="center"
+              justify="center"
+              flexWrap="wrap"
+              display="flex"
+              className={classes.gridMargin}
+            > */}
+            <Grid item xs={6}>
+              <Grid
+                container
+                spacing={3}
+                direction="row"
+                alignItems="center"
+                justify="center"
+                flexWrap="wrap"
+                display="flex"
+              >
+                <Card className={classes.card}>
+                  <CardContent>
+                    <CardMedia
+                      className={classes.media}
+                      image={profile.image}
+                      title="Profile image"
+                      style={{
+                        marginTop: "0.8em",
+                        marginBottom: "0.8em",
+                      }}
+                    />
+                    <Grid
+                      item
+                      direction="row"
+                      justify="flex-end"
+                      alignItems="center"
+                      spacing="0"
+                      style={{
+                        marginBottom: "0.8em",
+                      }}
+                    >
+                      <Link
+                        to="/profile/edit"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <Button
+                          variant="contained"
+                          endIcon={<FaIcons.FaUserEdit />}
+                          className={clsx(
+                            classes.successColor,
+                            classes.editButtonMargin
+                          )}
+                        >
+                          Edit My Profile
+                        </Button>
+                      </Link>
+                    </Grid>
+                    <Divider className={classes.divider} />
 
-	if (!userId) {
-		if (loading) return <Loading />;
-		_mybook = mybook.filter((book) => profile.userId === book.userId);
-		mycategories = categories
-			.filter((category) => {
-				return profile.myCategory.some((_category) => {
-					return _category.categoryId === category.id;
-				});
-			})
-			.map((category) => category.name);
-	} else {
-		if (otheProfileloading) return <Loading />;
-		else {
-			hisbook = mybook.filter((book) => otheProfile.userId === book.userId);
-			hiscategories = categories
-				.filter((category) => {
-					return otheProfile.hasCategory.some((_category) => {
-						return _category.categoryId === category.id;
-					});
-				})
-				.map((category) => category.name);
-		}
-	}
-	console.log(mycategories);
-	console.log(hiscategories);
-	return (
-		<>
-			{!userId ? (
-				<>
-					<>
-						<ViewRequest />
-						<Grid
-							container
-							spacing={0}
-							direction="row"
-							alignItems="center"
-							justify="center"
-							flexWrap="wrap"
-							display="flex"
-							className={classes.gridMargin}
-						>
-							<Grid item xs={6}>
-								<Card
-									container
-									spacing={3}
-									direction="row"
-									alignItems="center"
-									justify="center"
-									flexWrap="wrap"
-									display="flex"
-									className={clsx(classes.root, classes.card)}
-								>
-									<CardContent>
-										<CardMedia
-											className={classes.media}
-											image={profile.image}
-											title="Profile image"
-											style={{
-												marginTop: "0.8em",
-												marginBottom: "0.8em",
-											}}
-										/>
-										<Grid
-											item
-											direction="row"
-											justify="flex-end"
-											alignItems="center"
-											spacing="0"
-											style={{
-												marginBottom: "0.8em",
-											}}
-										>
-											<Link
-												to="/profile/edit"
-												style={{ textDecoration: "none" }}
-											>
-												<Button
-													variant="contained"
-													endIcon={<FaIcons.FaUserEdit />}
-													className={clsx(
-														classes.successColor,
-														classes.editButtonMargin
-													)}
-												>
-													Edit My Profile
-												</Button>
-											</Link>
-										</Grid>
-										<Divider className={classes.divider} />
-
-										<Typography
-											align="left"
-											color="primary"
-											variant="h6"
-											className={classes.userName}
-										>
-											Username: {profile.username}
-										</Typography>
+                    <Typography
+                      align="left"
+                      color="primary"
+                      variant="h6"
+                      className={classes.userName}
+                    >
+                      Username: {profile.username}
+                    </Typography>
 
                     <Grid
                       container
@@ -231,37 +232,35 @@ const Userprofile = () => {
                       // }}
                     ></Grid>
 
+                    <Typography
+                      align="left"
+                      color="primary"
+                      variant="h6"
+                      className={classes.userName}
+                    >
+                      Name: {profile.firstName} {profile.lastName}
+                    </Typography>
 
-										<Typography
-											align="left"
-											color="primary"
-											variant="h6"
-											className={classes.userName}
-										>
-											Name: {profile.firstName} {profile.lastName}
-										</Typography>
-
-
-										{/* <Divider className={classes.divider} /> */}
-										<Typography
-											align="left"
-											color="primary"
-											variant="h6"
-											className={classes.userName}
-										>
-											{`Email: ${profile.email}`}
-										</Typography>
-										<Typography
-											align="left"
-											color="primary"
-											variant="h6"
-											className={classes.userName}
-										>
-											{`Interested categories: ${mycategories}`}
-										</Typography>
-										<Divider className={classes.divider} />
-										<>
-											{/* <Grid
+                    {/* <Divider className={classes.divider} /> */}
+                    <Typography
+                      align="left"
+                      color="primary"
+                      variant="h6"
+                      className={classes.userName}
+                    >
+                      {`Email: ${profile.email}`}
+                    </Typography>
+                    <Typography
+                      align="left"
+                      color="primary"
+                      variant="h6"
+                      className={classes.userName}
+                    >
+                      {`Interested categories: ${mycategories}`}
+                    </Typography>
+                    <Divider className={classes.divider} />
+                    <>
+                      {/* <Grid
                         item
                         container
                         direction="row"
@@ -273,8 +272,8 @@ const Userprofile = () => {
                           marginBottom: "0.8em",
                         }}
                       > */}
-											{/* <div style={{ textDecoration: 'none' }> */}
-											{/* 
+                      {/* <div style={{ textDecoration: 'none' }> */}
+                      {/* 
                       <Link to="/viewrequest">
                         <Button
                           variant="contained"
@@ -289,69 +288,70 @@ const Userprofile = () => {
                           View Requests
                         </Button>
                       </Link> */}
-											{/* </Grid> */}
+                      {/* </Grid> */}
 
-											{/* </div> */}
-										</>
-										<Link>
-											<Link
-												to={`/books/new`}
-												style={{ textDecoration: "none" }}
-											>
-												<Button
-													variant="contained"
-													startIcon={<AddIcon />}
-													className={clsx(
-														classes.primaryColor,
-														classes.buttonMargin
-													)}
-												>
-													Add books
-												</Button>
-											</Link>
-										</Link>
-									</CardContent>
-								</Card>
-							</Grid>
-						</Grid>
-					</>
+                      {/* </div> */}
+                    </>
+                    <Link>
+                      <Link
+                        to={`/books/new`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        <Button
+                          variant="contained"
+                          startIcon={<AddIcon />}
+                          className={clsx(
+                            classes.primaryColor,
+                            classes.buttonMargin
+                          )}
+                        >
+                          Add books
+                        </Button>
+                      </Link>
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+            {/* </Grid> */}
+          </>
 
-					<div>
-						<Title>My Books</Title>
+          <div>
+            <Title>My Books</Title>
 
-						<BookList books={_mybook} />
-					</div>
-				</>
-			) : (
-				<>
-					<Grid
-						container
-						spacing={0}
-						direction="row"
-						alignItems="center"
-						justify="center"
-						flexWrap="wrap"
-						display="flex"
-						className={classes.gridMargin}
-					>
-						<Grid item xs={6}>
-							<Card
-								container
-								spacing={3}
-								direction="row"
-								alignItems="center"
-								justify="center"
-								flexWrap="wrap"
-								display="flex"
-								className={clsx(classes.root, classes.otherUserCard)}
-							>
-								<CardContent>
-									<CardMedia
-										className={classes.media}
-										image={otheProfile.image}
-										title="Profile image"
-									/>
-									{/* <Grid
+            <BookList books={_mybook} />
+          </div>
+        </>
+      ) : (
+        <>
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            alignItems="center"
+            justify="center"
+            flexWrap="wrap"
+            display="flex"
+            className={classes.gridMargin}
+          >
+            <Grid item xs={6}>
+              <Card
+                container
+                spacing={3}
+                direction="row"
+                alignItems="center"
+                justify="center"
+                flexWrap="wrap"
+                display="flex"
+                className={clsx(classes.root, classes.otherUserCard)}
+              >
+                <CardContent>
+                  <CardMedia
+                    className={classes.media}
+                    image={otheProfile.image}
+                    title="Profile image"
+                  />
+                  {/* <Grid
                     item
                     direction="row"
                     justify="flex-end"
@@ -364,77 +364,77 @@ const Userprofile = () => {
                       }
                     }
                   ></Grid> */}
-									<Divider className={classes.divider} />
+                  <Divider className={classes.divider} />
 
-									<Typography
-										align="left"
-										color="primary"
-										variant="h6"
-										className={classes.userName}
-									>
-										Username: {otheProfile.username}
-									</Typography>
+                  <Typography
+                    align="left"
+                    color="primary"
+                    variant="h6"
+                    className={classes.userName}
+                  >
+                    Username: {otheProfile.username}
+                  </Typography>
 
-									<Typography
-										align="left"
-										color="primary"
-										variant="h6"
-										className={classes.userName}
-									>
-										Name: {otheProfile.firstName} {otheProfile.lastName}
-									</Typography>
+                  <Typography
+                    align="left"
+                    color="primary"
+                    variant="h6"
+                    className={classes.userName}
+                  >
+                    Name: {otheProfile.firstName} {otheProfile.lastName}
+                  </Typography>
 
-									<Typography
-										align="left"
-										color="primary"
-										variant="h6"
-										className={classes.userName}
-									>
-										{`Email: ${otheProfile.email}`}
-									</Typography>
-									<Typography
-										align="left"
-										color="primary"
-										variant="h6"
-										className={classes.userName}
-									>
-										{`Interested categories: ${hiscategories}`}
-									</Typography>
-									<Divider className={classes.divider} />
-									<></>
-									<>
-										<Link
-											to={`/requests/new`}
-											style={{ textDecoration: "none" }}
-										>
-											<Button
-												variant="contained"
-												className={clsx(
-													classes.primaryColor,
-													classes.buttonMargin
-												)}
-												endIcon={<MessageIcon />}
-											>
-												Request
-											</Button>
-										</Link>
-									</>
-								</CardContent>
-							</Card>
-						</Grid>
-					</Grid>
+                  <Typography
+                    align="left"
+                    color="primary"
+                    variant="h6"
+                    className={classes.userName}
+                  >
+                    {`Email: ${otheProfile.email}`}
+                  </Typography>
+                  <Typography
+                    align="left"
+                    color="primary"
+                    variant="h6"
+                    className={classes.userName}
+                  >
+                    {`Interested categories: ${hiscategories}`}
+                  </Typography>
+                  <Divider className={classes.divider} />
+                  <></>
+                  <>
+                    <Link
+                      to={`/requests/new`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <Button
+                        variant="contained"
+                        className={clsx(
+                          classes.primaryColor,
+                          classes.buttonMargin
+                        )}
+                        endIcon={<MessageIcon />}
+                      >
+                        Request
+                      </Button>
+                    </Link>
+                  </>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
 
-					<div>
-						<Title>
-							{otheProfile.firstName} {otheProfile.lastName} Books
-						</Title>
+          <div>
+            <Title>
+              {otheProfile.firstName} {otheProfile.lastName} Books
+            </Title>
 
-						<BookList books={hisbook} />
-					</div>
-				</>
-			)}
-		</>
-	);
+            <BookList books={hisbook} />
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Userprofile;
